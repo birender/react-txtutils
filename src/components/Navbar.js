3df887fc77b4f53ce849362e2f9a364c29db6a7b
Birender Singh Rana <birender.rana18@gmail.com>
2024-05-13 16:09:33 +0530

    Add NavLink Module
    - Add Navlink Module for navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { NavLink  } from "react-router-dom";
 
 export default function Navbar(props) {
   return ( 
@@ -11,19 +12,19 @@ export default function Navbar(props) {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <li className="nav-item">               
+                <NavLink exact to="/" className="nav-link">Home</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">{props.aboutText}</a>
+                <NavLink to="/about" className="nav-link">{props.aboutText}</NavLink>
               </li>
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Dropdown
                 </a>
                 <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="/">Action</a></li>
-                  <li><a className="dropdown-item" href="/">Another action</a></li>
+                  <li><a  className="dropdown-item" href="/">Action</a></li>
+                  <li><a  className="dropdown-item" href="/about">Another action</a></li>
                   <li><hr className="dropdown-divider"/></li>
                   <li><a className="dropdown-item" href="/">Something else here</a></li>
                 </ul>
