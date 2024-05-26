4d45a74c51039e2d5f642c2368bc37575d173b33
birender <birender.rana18@gmail.com>
2024-05-26 11:26:49 +0530

    Updated - Added Spinner Componets
    - Added Spinner Componets
    - Clear Previous Result click on next button
    - Disabled Button Based on Pagenation
    -	modified:   src/components/News.js
    -	new file:   src/components/Spinner.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
+import Spinner from './Spinner';
 
 export class News extends Component {
     constructor() {
@@ -28,7 +29,7 @@ export class News extends Component {
     }
 
     handleNextNews = async ()=>{   
-              
+        this.setState({page:1,loading: true}) ; 
         let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=1aafdb26827c468ba763e89b7a6828e8&page=${this.state.page+1}`;
         let data = await fetch(url);
         let parseData = await data.json(); 
@@ -43,6 +44,7 @@ export class News extends Component {
 
     handlePrevNews = async ()=>{
         if( this.state.page > 1){
+            this.setState({page:1,loading: true}) ;
             let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=1aafdb26827c468ba763e89b7a6828e8&page=${this.state.page-1}`;
             let data = await fetch(url);
             let parseData = await data.json();        
@@ -53,22 +55,23 @@ export class News extends Component {
                 pagenation : Math.ceil(this.state.totalResults / this.state.articles.length)
             });
         }else{
-            this.setState({page:1}) ;
+            this.setState({page:1,loading: true}) ;
         }
     }
 
     render() {
         return (
             <>
-                <h2>Top headlines</h2>
+                <h2>Top headlines</h2>                
                 <div className="Container">
+                { this.state.loading && (<Spinner/>)}
                     <div className="d-flex justify-content-between">
                         <button disabled={this.state.page<=1} type="button" onClick={this.handlePrevNews} className="btn btn-dark">&larr;Previous</button>
                         <button disabled={this.state.page >= this.state.pagenation} type="button" onClick={this.handleNextNews} className="btn btn-dark">Next &rarr;</button>
                     </div>
                 </div>
                 <div className="row">
-                    {this.state.articles.map((element) => {
+                    {!this.state.loading && this.state.articles.map((element) => {
                         let title = element.title == null ? '' : element.title;
                         let description = element.description == null ? element.title : element.description;
                         let imageURL = element.urlToImage == null ? '' : element.urlToImage;
---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Spinner extends Component {
+  render() {
+    return (
+      <>
+        <div className="spinner-border text-primary text-center" role="status"></div>
+      </>
+    )
+  }
+}
+
+export default Spinner
