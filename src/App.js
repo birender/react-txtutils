35f2553ae71d0024d546670fb239028e8a111755
Birender Singh Rana <birender.rana18@gmail.com>
2024-05-27 16:58:44 +0530

    Dynamic News API Key
    Dynamic News API Key

--- src/App.js
+++ src/App.js
@@ -7,20 +7,19 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export class App extends Component {
   render() {
+    const apikey = '6ebe665750794121a3eab86cfaedeff8'; // Old 1aafdb26827c468ba763e89b7a6828e8
     return (
       <Router>
         <Navbar />
         <div className="container my-3">
-          <Routes>
-          
-            <Route exact path="/" element={<News key="general" category="General" />} />            
-            <Route exact path="/business" element={<News key="business" category="business" />} />
-            <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment" />} />
-            <Route exact path="/health" element={<News key="health" category="health" />} />
-            <Route exact path="/science" element={<News key="science" category="science" />} />
-            <Route exact path="/sports" element={<News key="sports" category="sports" />} />
-            <Route exact path="/technology" element={<News key="technology" category="technology" />} />
-          
+          <Routes>        
+            <Route exact path="/" element={<News key="general" category="General" apikey={apikey} />} />            
+            <Route exact path="/business" element={<News key="business" category="business" apikey={apikey}/>} />
+            <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment" apikey={apikey}/>} />
+            <Route exact path="/health" element={<News key="health" category="health" apikey={apikey}/>} />
+            <Route exact path="/science" element={<News key="science" category="science" apikey={apikey}/>} />
+            <Route exact path="/sports" element={<News key="sports" category="sports" apikey={apikey}/>} />
+            <Route exact path="/technology" element={<News key="technology" category="technology" apikey={apikey}/>} />        
           </Routes>
         </div>
       </Router>
