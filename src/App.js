df70e7e8dea607d37359822f9e7c4d41c368b017
Birender Singh Rana <birender.rana18@gmail.com>
2024-05-28 14:41:56 +0530

    Adding Module
    - Added Infinite Module
    - Added Loaded Module
    - Set Enviroment Variable

--- src/App.js
+++ src/App.js
@@ -4,22 +4,32 @@ import React, { Component } from 'react'
 import Navbar from './components/NavBar';
 import News from './components/News';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
 export class App extends Component {
+  state = {
+    progress : 0
+  };
+  
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
+
   render() {
-    const apikey = '6ebe665750794121a3eab86cfaedeff8'; // Old 1aafdb26827c468ba763e89b7a6828e8
+    const apikey = process.env.REACT_APP_NEWS_API_KEY; // Old 1aafdb26827c468ba763e89b7a6828e8    
     return (
       <Router>
-        <Navbar />
+        <LoadingBar color='#f11946' progress={this.state.progress}/>
+        <Navbar />        
         <div className="container my-3">
           <Routes>        
-            <Route exact path="/" element={<News key="general" category="General" apikey={apikey} />} />            
-            <Route exact path="/business" element={<News key="business" category="business" apikey={apikey}/>} />
-            <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment" apikey={apikey}/>} />
-            <Route exact path="/health" element={<News key="health" category="health" apikey={apikey}/>} />
-            <Route exact path="/science" element={<News key="science" category="science" apikey={apikey}/>} />
-            <Route exact path="/sports" element={<News key="sports" category="sports" apikey={apikey}/>} />
-            <Route exact path="/technology" element={<News key="technology" category="technology" apikey={apikey}/>} />        
+            <Route exact path="/" element={<News setProgress={this.setProgress} key="general" category="General" apikey={apikey} />} />            
+            <Route exact path="/business" element={<News setProgress={this.setProgress} key="business" category="business" apikey={apikey}/>} />
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} key="entertainment" category="entertainment" apikey={apikey}/>} />
+            <Route exact path="/health" element={<News setProgress={this.setProgress} key="health" category="health" apikey={apikey}/>} />
+            <Route exact path="/science" element={<News setProgress={this.setProgress} key="science" category="science" apikey={apikey}/>} />
+            <Route exact path="/sports" element={<News setProgress={this.setProgress}  key="sports" category="sports" apikey={apikey}/>} />
+            <Route exact path="/technology" element={<News setProgress={this.setProgress} key="technology" category="technology" apikey={apikey}/>} />        
           </Routes>
         </div>
       </Router>
